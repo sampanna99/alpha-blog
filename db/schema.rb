14d86bdcfc3fb4232d71af199f0c224ba7e778c4
sampanna99 <sampannapokhrel@gmail.com>
2016-06-23 06:57:13 +0000

    uptotesting listing categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621160513) do
+ActiveRecord::Schema.define(version: 20160622145048) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160621160513) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chalaune_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "chalaunes", force: :cascade do |t|
