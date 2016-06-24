03187fab54239ed802d9e263378f245eab0530c8
sampanna99 <sampannapokhrel@gmail.com>
2016-06-24 06:21:21 +0000

    add association from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,7 +43,7 @@ end
   end
   private
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
   def set_params
     @article = Article.find(params[:id])
